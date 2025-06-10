6e5d23da1dd61387067d3a80a7b44dc970d5f394
Valentina <valentina.lopez02@alumnos.ucn.cl>
2025-06-09 21:25:01 -0400

    mqtt

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.MQTT,
+    options: {
+      url: 'mqtt://localhost:1883', // Cambia si tienes otro host o puerto
+    },
+  });
+
+  await app.startAllMicroservices();
+  await app.listen(3000);
 }
 bootstrap();
--- src/mediciones/entities/medicion.entity.ts
+++ src/mediciones/entities/medicion.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
 @Entity()
 export class Medicion {
